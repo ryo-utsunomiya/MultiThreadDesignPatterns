ef273b0e2ab8d97735f5d109998c481b23316d11
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-06-18 21:22:40 +0900

    Init

---
+++ Introduction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Introduction
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var print = new Print("Good!");
+            var thread = new Thread(print.Run);
+            thread.Start();
+        }
+    }
+
+    internal class Print
+    {
+        private readonly string _message;
+
+        public Print(string message)
+        {
+            _message = message;
+        }
+
+        public void Run()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(_message);
+            }
+        }
+    }
+}
